a52c1988de1c4239424c209cccdbee31543c2b08
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2024-04-12 12:35:27 +0530

    Initial Setup done

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <main>
+        <h1>Hero-Verse</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
